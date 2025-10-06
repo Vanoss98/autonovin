8de66208d9f32428a9767ad9bfa74167e2cd2455
Vanoss_g98 <amirsaman.shohrati@gmail.com>
2025-10-06 15:57:34 +0330

    made rag more efficient

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ChatAPIView, ChatDebugAPIView
+from .views import ChatAPIView, ChatHistoryAPIView
 
 urlpatterns = [
     path("chat/", ChatAPIView.as_view(), name="chat"),
-    path("api/chat/debug/", ChatDebugAPIView.as_view(), name="chat-debug"),
-    # path("get-history/<str:thread_id>", ChatHistoryAPIView.as_view(), name="get-history")
+    # path("api/chat/debug/", ChatDebugAPIView.as_view(), name="chat-debug"),
+    path("get-history/<str:thread_id>", ChatHistoryAPIView.as_view(), name="get-history")
 ]
